540f7e4e7718c1768b5c9e84f9fb966dc9ea84fc
Paul Banyai <pjbanyai@gmail.com>
2018-02-09 16:48:37 -0800

    Fixed invisible soda & starting level for Unity 5.4+
    I fixed the drink prefab which was hidden on the wrong layer. Thanks to comments on the tutorial's video shown on Youtube I was able to fix the starting level for Unity 5.4+.

--- 2d-roguelike-PB/Assets/Scripts/GameManager.cs
+++ 2d-roguelike-PB/Assets/Scripts/GameManager.cs
@@ -20,7 +20,7 @@ public class GameManager : MonoBehaviour
 
 	private Text levelText;                                 //Text to display current level number.
 	private GameObject levelImage;                          //Image to block out level as levels are being set up, background for levelText.
-	private int level = 1;                                  //Current level number, expressed in game as "Day 1".
+	private int level = 0;                                  //Current level number, expressed in game as "Day 1".
 	private List<Enemy> enemies;                          //List of all Enemy units, used to issue them move commands.
 	private bool enemiesMoving;                             //Boolean to check if enemies are moving.
 	private bool doingSetup = true;                         //Boolean to check if we're setting up board, prevent Player from moving during setup.
@@ -49,8 +49,14 @@ public class GameManager : MonoBehaviour
 		//Get a component reference to the attached BoardManager script
 		boardScript = GetComponent<BoardManager>();
 		
-		//Call the InitGame function to initialize the first level 
-		InitGame();
+		//Get a component reference to the attached BoardManager script
+		boardScript = GetComponent<BoardManager>();
+
+		#if ( UNITY_4 || UNITY_5_0 || UNITY_5_1 || UNITY_5_2 || UNITY_5_3)	
+			level = 1;
+			//Call the InitGame function to initialize the first level 
+			InitGame();
+		#endif
 	}
 
 	#if ( UNITY_4 || UNITY_5_0 || UNITY_5_1 || UNITY_5_2 || UNITY_5_3)
