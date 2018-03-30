5208b886ccc06ab4d0e4e5d1a122cc41f9aa9585
zeknock <35779251+zeknock@users.noreply.github.com>
2018-03-29 23:15:51 -0700

    a8

---
+++ Assignment 8/Ass8_bina/Assets/Scripts/mouseover.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class mouseover : MonoBehaviour {
+
+	public Text ESCtext;
+
+	void Start(){
+		
+		ESCtext.text = "";
+	}
+
+	void Update(){
+		if (Input.GetMouseButtonDown(0)) {
+			ESCtext.text = "Press the ESC key to press the button";
+		}
+	}
+
+	void OnTriggerEnter(Collider other){
+
+		if(other.gameObject.CompareTag("Astronaut")){
+			ESCtext.text = "Press the ESC key to press the button";
+		}
+	}
+}
---
+++ Assignment 8/Ass8_bina/Assets/Trigger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;using UnityEngine.UI;
+
+public class Trigger : MonoBehaviour {
+
+	public Text ESCtext;
+
+	void Start(){
+
+		ESCtext.text = "";
+	}
+
+
+/*	void OnTriggerEnter(Collider other){
+
+		if(other.gameObject.CompareTag("Astronaut")){
+			ESCtext.text = "Press the ESC key to press the button";
+		}
+	}*/
+
+
+	void OnTriggerEnter(Collider other) {
+
+		if(other.gameObject.tag == "Astronaut"){
+			ESCtext.text = "Press the ESC key to press the button";
+		}
+
+	}
+
+	void OnTriggerExit(Collider other){
+		ESCtext.text = "";
+	}
+}
