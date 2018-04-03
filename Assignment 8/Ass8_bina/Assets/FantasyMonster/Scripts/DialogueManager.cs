f5b9e177bee41b4ef009b170ff9778a4937538ce
unknown <chase.mcintyre105@myci.csuci.edu>
2018-04-03 15:27:07 -0700

    Assignment 8 Complete

---
+++ Assignment 8/Ass8_bina/Assets/FantasyMonster/Scripts/DialogueManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class DialogueManager : MonoBehaviour {
+
+
+	public GameObject DialogueBox;
+	public Text DialougueText;
+	public RigidbodyFirstPersonController player;
+
+	public bool dialogueActive;
+	public bool talkingToSkeleton;
+	public int currentline;
+	public string[] dialoguelines;
+
+
+	// Use this for initialization
+	void Start () {
+		DialogueBox.SetActive (false);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		DialougueText.text = dialoguelines [currentline];
+	}
+
+	void OnTriggerEnter(Collider other) {
+
+		if(other.gameObject.tag == "DialogueTrigger1"){
+			currentline = 0;
+			dialogueActive = true;
+			talkingToSkeleton = true;
+			DialogueBox.SetActive(true);
+		}
+
+	}
+
+	void OnTriggerExit(Collider other){
+		talkingToSkeleton = false;
+		DialogueBox.SetActive (false);
+	}
+
+
+}
