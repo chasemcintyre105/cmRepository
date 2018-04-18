39036eea9c1064878cd9e916f7c4be81035901a7
zeknock <35779251+zeknock@users.noreply.github.com>
2018-04-17 17:28:29 -0700

    just leaving a note it's not like I didn't do anything T.T

---
+++ Term Project/User Interface/chessgame/Assets/Arcane Cyber/Chess/script/Chessman.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chessman : MonoBehaviour {
+
+	public int CurrentX{set; get;}
+	public int CurrentY{set; get;}
+
+	public bool isWhite;
+
+	public void SetPosition (int x , int y){
+
+		CurrentX = x;
+		CurrentY = y;
+
+	}
+
+	public virtual bool[,] PossibleMove(){
+		
+		return new bool[8,8];
+	}
+}
